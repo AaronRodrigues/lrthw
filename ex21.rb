58ba0aef975187785bfb76263a30867ab949c2c6
Aaron <aaronrod@gmail.com>
2017-04-13 20:35:51 +0100

    Added comments and extra work to ex21

--- ex21.rb
+++ ex21.rb
@@ -14,7 +14,7 @@ def multiply(a, b)
 end
 
 def divide(a, b)
-	puts "dividing #{a} / #{b}"
+	puts "DIVIDING #{a} / #{b}"
 	return a / b
 end
 
@@ -33,4 +33,10 @@ puts "Here is a puzzle."
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
-puts "That becomes: #{what}. Can you do it by hand?"
+puts "That becomes: #{what}. Can you do it by hand?"	# Calculated  by hand, it is correct
+
+puts "What is 24 + 34 / 100 - 1023?"					#tried another formula
+
+ans = subtract(  add(24, divide(34, 100)), 1023)		
+
+puts "The answer is #{ans}"
